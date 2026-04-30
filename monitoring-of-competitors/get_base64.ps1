3fef71cb376d9a932489db3c025a3be45107d21c
Hryhorillia <Hryhorillia@ukr.net>
2026-04-30 22:04:10 +0200

    Cleanup: Remove legacy scripts, drafts and AI service files from monitoring project

--- monitoring-of-competitors/get_base64.ps1
+++
@@ -1,4 +0,0 @@
-$bytes = [System.IO.File]::ReadAllBytes('[USER_HOME]\OneDrive\Work.Oki-toki\Monitoring of competitors\update_format_nodes.json')
-$text = [System.Text.Encoding]::UTF8.GetString($bytes)
-[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($text))
-
